f18f04acff8053ff294a54c3b8b4fe2a0a03f629
suraj <surajraj.shku1999@gmail.com>
2025-05-03 18:03:02 +0530

    database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
-    await mongoose.connect(process.env.DB_CONNECTION_SECRET);
+    await mongoose.connect("mongodb+srv://suraj:zDxJTbqzS83pTtKe@cluster0.ajzily6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 }
 export default connectDB;
